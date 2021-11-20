d7ab6f7e2c123f00cc777cdb99939c661752a931
abishek2008 <69070114+abishek2008@users.noreply.github.com>
2021-11-20 16:34:06 +0530

    Add files via upload

---
+++ datavisualization.py
@@ -0,0 +1,10 @@
+#importing the main two
+
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("Copy+of+data+-+data.csv")
+
+fig = px.scatter(df,x = "date",y = "cases",color = "country",size_max = 60)
+
+fig.show()
